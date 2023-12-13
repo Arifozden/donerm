5ec12dbc3b8ea6fee5340e13b9f46d577bd40645
arifozden <arifozden1@gmail.com>
2023-12-13 01:22:30 +0100

    some problems

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,32 @@
+import { FaSearch } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+
+export default function Header() {
+  return (
+    <header className='bg-blue-200 shadow-md'>
+        <div className='flex justify-between items-center max-w-6xl mx-auto p-2'>
+            <Link to='/'>
+        <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
+            <span className='text-slate-500'>Doner</span>
+            <span className='text-slate-700'>Meg</span>
+        </h1>
+        </Link>
+        <form className='bg-blue-100 p-2 rounded-md flex items-center'>
+            <input type='text' placeholder='Search...' className='bg-transparent focus:outline-none w-24 sm:w-64 placeholder:text-blue-400'/>
+            <FaSearch className='text-blue-400' />
+        </form>
+        <ul className='flex gap-4'>
+            <Link to='/products'>
+            <li className='font-medium text-blue-500 hover:underline'>Products</li>
+            </Link>
+            <Link to='/about'>
+            <li className='hidden font-medium sm:inline text-blue-500 hover:underline'>About</li>
+            </Link>
+            <Link to='sign-in'>
+            <li className='font-medium  text-blue-500 hover:underline'>Sign In</li>
+            </Link>
+        </ul>
+        </div>
+    </header>
+  );
+}
