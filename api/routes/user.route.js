81189283a8f63c96194c2c0f324ba159c749d260
arifozden <arifozden1@gmail.com>
2023-12-16 23:31:18 +0100

    api-user update created

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
+import { verifyToken } from '../utils/verifyUser.js';
+import {test, updateUser} from '../controllers/user.controller.js';
 
 const router = express.Router();
 
-router.get('/test', (req, res)=> {
-    res.send('Hello!');
-});
+router.get('/test', test);
+router.post('/update/:id', verifyToken, updateUser);
 
 export default router;
