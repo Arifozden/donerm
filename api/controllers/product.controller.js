a220916a82adb6fa5913417f84c5891eeec2a6b9
arifozden <arifozden1@gmail.com>
2023-12-21 22:14:52 +0100

    api-update product route created

--- api/controllers/product.controller.js
+++ api/controllers/product.controller.js
@@ -1,4 +1,5 @@
 import Product from '../models/product.model.js';
+import { errorHandler } from '../utils/error.js';
 
 export const createProduct = async (req, res, next) => {
     try {
@@ -19,6 +20,21 @@ export const deleteProduct = async (req, res, next) => {
     try {
         await Product.findByIdAndDelete(req.params.id);
         res.status(200).json("Product deleted successfully");
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const updateProduct = async (req, res, next) => {
+    const product = await Product.findById(req.params.id);
+
+    if(!product) return next(errorHandler(404, "Product not found"));
+
+    if(req.user.id !== product.userRef) return next(errorHandler(401, "You can update only your own products"));
+
+    try {
+        const updatedProduct = await Product.findByIdAndUpdate(req.params.id, req.body, {new: true});
+        res.status(200).json(updatedProduct);
     } catch (error) {
         next(error);
     }
