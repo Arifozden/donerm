cea5981ee591074ea30bf91cb7d63aa86cdfab0a
arifozden <arifozden1@gmail.com>
2023-12-23 00:45:43 +0100

    product page completed

--- client/src/pages/Product.jsx
+++ client/src/pages/Product.jsx
@@ -8,6 +8,12 @@ import { useParams } from 'react-router-dom';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import {
+    FaEnvelopeSquare,
+    FaMapMarkerAlt,
+    FaMobileAlt,
+    FaUserAlt,
+  } from 'react-icons/fa';
 
 export default function Product() {
     //SwiperCore.use([Navigation]);
@@ -15,6 +21,19 @@ export default function Product() {
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(false);
     const params = useParams();  
+
+    const [isModalOpen, setModalOpen] = useState(false);
+  const [selectedImage, setSelectedImage] = useState('');
+
+  const handleImageClick = (image) => {
+    setSelectedImage(image);
+    setModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+    setSelectedImage('');
+  };
 
     useEffect(() => {
         const fetchProduct = async () => {
@@ -47,25 +66,68 @@ fetchProduct();
         slidesToScroll: 1,
       };
 
+      let colorClass;
+
+  // Duruma bağlı olarak renk sınıfını belirle
+  if (!product || !product.condition) {
+    colorClass = 'bg-white-900 w-full max-w-[200px] text-white text-center rounded-md';
+  } else if (product.condition === 'New') {
+    colorClass = 'bg-green-900 w-full max-w-[200px] text-white text-center rounded-md';
+  } else if (product.condition === 'Used') {
+    colorClass = 'bg-yellow-500 w-full max-w-[200px] text-white text-center rounded-md';
+  } else {
+    colorClass = 'bg-yellow-700 w-full max-w-[200px] text-white text-center rounded-md';
+  }
+
   return (
-     <main className="flex flex-col items-center justify-center">
-        {loading && <p className='text-center my-7 text-2xl'>Loading...</p>}
-        {error && (<p className='text-center my-7 text-2xl'>Something went wrong!</p>)}
-        {product && !loading && !error && (
-        <>
+    <main className="flex flex-col items-center justify-center pt-5">
+    {loading && <p className='text-center my-7 text-2xl'>Loading...</p>}
+    {error && <p className='text-center my-7 text-2xl'>Something went wrong!</p>}
+    {product && !loading && !error && (
+      <>
         <div className="flex">
-      {product.images.map((image, index) => (
-        <img
-          key={index}
-          src={image}
-          alt={product.title}
-          className="w-32 h-32 object-cover mr-2"
-        />
-      ))}
-    </div><h1>{product.title}</h1>
-        
-        </>)}
-        
+          {product.images.map((image, index) => (
+            <img
+              key={index}
+              src={image}
+              alt={product.title}
+              className="w-32 h-32 object-cover mr-2 cursor-pointer"
+              onClick={() => handleImageClick(image)}
+            />
+          ))}
+        </div>
+        <div>
+        <p className='text-2xl font-semibold uppercase pt-4 pb-2'>{product.title}</p>
+        <p className='bg-green-900 w-full max-w-[200px] text-white text-center p-2 rounded-md'>{product.price} kr</p>
+          
+        </div>
+
+        {isModalOpen && (
+          <div className="fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center">
+            <div className="bg-white p-4 max-w-md">
+              <img src={selectedImage} alt={product.title} />
+              <button onClick={closeModal}>Close</button>
+            </div>
+            
+          </div>
+        )}
+        <div className='flex pt-3'>
+  <span className='font-semibold pr-2'>Condition:</span>
+  {product && (
+    <p className={`text-slate-800 ${colorClass} pl-2`}>{product.condition}</p>
+  )}
+</div>
+        <p className='text-slate-800 pt-10'>
+                <span className='font-semibold'>Description:</span> {product.description} <br/>
+            </p>
+        <ul>
+            <li className='flex items-center mt-6 gap-2 text-slate-600  text-sm pt-3'><FaMapMarkerAlt className='text-green-700' />{product.address}</li>
+            <li className='flex items-center mt-6 gap-2 text-slate-600  text-sm pt-3'><FaUserAlt className='text-green-700' />{product.userRef}</li>
+            <li className='flex items-center mt-6 gap-2 text-slate-600  text-sm pt-3'><FaMobileAlt className='text-green-700' />{product.phone}</li>
+            <li className='flex items-center mt-6 gap-2 text-slate-600  text-sm pt-3'><FaEnvelopeSquare className='text-green-700' /><a href={`mailto:${product.email}`}>{product.email}</a></li>
+        </ul>
+      </>
+    )}
   </main>
   );
 }
