23c6387c0367198d5e227ba3c3ad12a68f56f505
arifozden <arifozden1@gmail.com>
2023-12-26 02:16:17 +0100

    about page completed

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,51 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-900'>About DonerMeg</h1>
+      <h2 className='text-2xl font-bold mb-4 text-slate-900'>Help with Happiness!</h2>
+      <h3 className='text-xl font-bold mb-4 text-slate-900'>What is DonerMeg?</h3>
+      <p className='mb-4 text-slate-800'>
+        DonerMeg is a web application that allows users to donate their unwanted items to those in need.
+      </p>
+      <p className='mb-4 text-slate-800'>
+      <span className='font-semibold'>What problem are you trying to solve?</span> <br />
+
+There is a need for a new concept in the name of charity and volunteerism.
+
+      </p>
+      <p className='mb-4 text-slate-800'><span className='font-semibold'>
+      How are you trying to solve this problem?<br /></span>
+
+People in Norway like to help others.<br/>
+In addition, hundreds of products change hands on sales sites every day for free or at low prices. <br/>
+This is an idea based on combining these two.<br/>
+People will sell the items they would give for free or for low prices on the site at prices they determine, and they will donate the money from this sale to a humanitarian aid organization.
+
+      </p>
+      <p className='mb-4 text-slate-800'><span className='font-semibold'>
+      How are you going to make money?</span><br />
+
+With this application, the primary goal is to be a bridge between people and humanitarian aid organizations rather than generating income.<br/>
+
+In addition to small commissions from these purchases, support can also be expected from the organizations to which the donations will be transferred.
+
+      </p>
+      <p className='mb-4 text-slate-800'><span className='font-semibold'>
+      Is this an exponentially growing industry? Who are your main competitors and how much of the market can you capture? What is your competitive advantage?</span>
+      <br />
+As far as I can see, different works are being carried out in this field, but there is no such comprehensive and large work. 
+<br/>That's why I think it will be the first in its field 
+
+      </p>
+      <p className='mb-4 text-slate-800'><span className='font-semibold'>
+      What makes you the right team to solve this problem? What's your unique insight?</span><br />
+
+At the moment, this thought is an individual thought.<br/>
+I need your guidance on what the team structure will be like before we start implementing it.
+
+      </p>
+      <p className='mb-4 text-slate-800 text-xs'>The application is built using the MERN stack, with Redux for state management.</p>
+    </div>
   )
 }
